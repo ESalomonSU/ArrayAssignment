281c308812e10dff4b5048f3f8bf45f5703b4dfa
ESalomonSU <74840611+ESalomonSU@users.noreply.github.com>
2021-10-02 23:45:00 -0600

    Add files via upload
    I tried to figure out how to do it in reverse but I couldn't understand it, I hope I can correct it tomorrow but for now I just don't want to be left without delivering anything.

---
+++ Array copy/Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 3, 999, 2, 1, 5 };
+
+            for(int i = 0; i < myArray.Length; i++)
+            {
+                for(int j=i+1; j < myArray.Length; j++)
+                {
+                    if (myArray[i] > myArray[j])
+                    {
+                        int temp = 0;
+                        temp = myArray[j];
+                        myArray[j] = myArray[i];
+                        myArray[i] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
